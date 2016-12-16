a55f8b6dd7e45d78ef3e63d4bc175afbdcd2158e
Cristian Steib <steibkhriz@gmail.com>
2016-12-16 06:12:45 -0300

    new

--- scheduler.py
+++ scheduler.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 '''
     Created by: Cristian Steib
     date: 16/12/16
@@ -15,6 +16,7 @@ class Time():
 
     def __init__(self):
         self.lapse = 0
+        self.crontab = ('*', '*', '*', '*', '*')
 
     def set_lapse(self, time):
         self.lapse = time
@@ -23,6 +25,46 @@ class Time():
         return self.time
 
     lapse = property(get_lapse, set_lapse)
+
+    def set_crontab(self, minute='*', hour='*', day='*', month='*', week_day='*'):
+        '''
+        Like linux crontab
+
+        .--------------- minuto (0-59)
+        |  .------------ hora (0-23)
+        |  |  .--------- día del mes (1-31)
+        |  |  |  .------ mes (1-12) o jan,feb,mar,apr,may,jun,jul... (meses en inglés)
+        |  |  |  |  .--- día de la semana (0-6) (domingo=0 ó 7) o sun,mon,tue,wed,thu,fri,sat (días en inglés)
+        |  |  |  |  |
+        *  *  *  *  *
+
+        '''
+        week_day = str(week_day)
+
+        days = {0: ['sun', 'dom', '0', '7'],
+                1: ['mon', 'lun', '1'],
+                2: ['tue', 'mar', '2'],
+                3: ['wed', 'mie' '3'],
+                4: ['thu', 'jue', '4'],
+                5: ['fri', 'vie' '5'],
+                6: ['sat', 'sab' '6'],
+                }
+        wk_day = False
+        for key in days:
+            for item in days[key]:
+                if week_day == item:
+                    wk_day = key
+                    break
+            print wk_day
+            if wk_day == False:
+                break
+
+        self.set_crontab(str(minute), str(hour), str(day), str(month), str(wk_day))
+
+    def get_crontab(self):
+        return self.crontab
+
+    crontab = property(set_crontab, get_crontab)
 
 
 class JobThread(threading.Thread):
@@ -74,6 +116,8 @@ class Schedule():
         job.job(function, args, kwargs)
         self.jobs.append(job)
 
+        return job
+
     def get_jobs(self):
         return self.jobs
 
@@ -84,6 +128,6 @@ def daemon():
 
 
 c = Schedule()
-c.addJob(daemon)
+print c.addJob(daemon).every().lapse
 
 print ' exit main'
