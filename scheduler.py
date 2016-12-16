a404753c1c6bb8423e6d639f1d811c886c7b663d
Cristian <steibkhriz@gmail.com>
2016-12-16 07:28:30 -0300

    ADD files

--- scheduler.py
+++ scheduler.py
@@ -15,16 +15,23 @@ class Time():
     '''
 
     def __init__(self):
-        self.lapse = 0
-        self.crontab = ('*', '*', '*', '*', '*')
+        self.lapse = '0'
 
-    def set_lapse(self, time):
-        self.lapse = time
+        self._minute = '*'
+        self._hour = '*'
+        self._day = '*'
+        self._month = '*'
+        self._week_day = '*'
+        self.crontab = (self._minute, self._hour, self._day, self._month, self._week_day)
+
+    def set_lapse(self, time='0'):
+        self.lapse = str(time)
+        return self
+
 
     def get_lapse(self):
-        return self.time
+        return self.lapse
 
-    lapse = property(get_lapse, set_lapse)
 
     def set_crontab(self, minute='*', hour='*', day='*', month='*', week_day='*'):
         '''
@@ -34,32 +41,107 @@ class Time():
         |  .------------ hora (0-23)
         |  |  .--------- día del mes (1-31)
         |  |  |  .------ mes (1-12) o jan,feb,mar,apr,may,jun,jul... (meses en inglés)
-        |  |  |  |  .--- día de la semana (0-6) (domingo=0 ó 7) o sun,mon,tue,wed,thu,fri,sat (días en inglés)
+        |  |  |  |  .--- día de la semana (0-6) (domingo=0 ó 7) o sun,mon,tue,wed,thu,fri,sat (días en inglés y español)
         |  |  |  |  |
         *  *  *  *  *
 
         '''
-        week_day = str(week_day)
 
-        days = {0: ['sun', 'dom', '0', '7'],
-                1: ['mon', 'lun', '1'],
-                2: ['tue', 'mar', '2'],
-                3: ['wed', 'mie' '3'],
-                4: ['thu', 'jue', '4'],
-                5: ['fri', 'vie' '5'],
-                6: ['sat', 'sab' '6'],
-                }
-        wk_day = False
-        for key in days:
-            for item in days[key]:
-                if week_day == item:
-                    wk_day = key
+        self._minute = minute if minute != '*' else self._minute
+        self._hour = hour if hour != '*' else self._hour
+        self._day = day if day != '*' else self._day
+        self._month = month if month != '*' else self._month
+        self._week_day = week_day if week_day != '*' else self._week_day
+
+        if week_day != '*':
+            week_day = str(week_day)
+            days = {0: ['sun', 'dom', '0', '7'],
+                    1: ['mon', 'lun', '1'],
+                    2: ['tue', 'mar', '2'],
+                    3: ['wed', 'mie' '3'],
+                    4: ['thu', 'jue', '4'],
+                    5: ['fri', 'vie' '5'],
+                    6: ['sat', 'sab' '6'],
+                    }
+
+            wk_day = False
+            for key in days:
+                for item in days[key]:
+                    if week_day == item:
+                        self._week_day = key
+                        break
+                if wk_day != False:
                     break
-            print wk_day
-            if wk_day == False:
-                break
 
-        self.set_crontab(str(minute), str(hour), str(day), str(month), str(wk_day))
+        self.crontab = (str(self._minute), str(self._hour), str(self._day), str(self._month), str(self._week_day))
+
+    def get_crontab(self):
+        return self.crontab
+
+    crontab = property(set_crontab, get_crontab)
+
+    @property
+    def monday(self):
+        self.set_crontab(week_day='mon')
+        return self
+
+    @property
+    def tuesday(self):
+        self.set_crontab(week_day='tue')
+        return self
+
+    @property
+    def wednesday(self):
+        self.set_crontab(week_day='wed')
+        return self
+
+    @property
+    def monday(self):
+        self.set_crontab(week_day='mon')
+        return self
+
+    @property
+    def thurday(self):
+        self.set_crontab(week_day='thu')
+        return self
+
+    @property
+    def friday(self):
+        self.set_crontab(week_day='fri')
+        return self
+
+    @property
+    def saturday(self):
+        self.set_crontab(week_day='sat')
+        return self
+
+    @property
+    def sunday(self):
+        self.set_crontab(week_day='sun')
+        return self
+
+    @property
+    def seconds(self):
+        self.set_crontab(minute='*/0.' + self.lapse)
+        return self
+
+    @property
+    def minutes(self):
+        self.set_crontab(minute='*/' + self.lapse)
+        return self
+
+    @property
+    def hours(self):
+        self.set_crontab(minute='*/' + self.lapse)
+        return self
+
+    def day(self):
+        pass
+
+    def at(self, time="00:00"):
+        time = time.split(':')
+        self.set_crontab(hour=time[0], minute=time[1])
+
 
     def get_crontab(self):
         return self.crontab
@@ -92,7 +174,7 @@ class Job():
         return self.job.isAlive()
 
     def every(self, time=1):
-        return self.time
+        return self.time.set_lapse(time)
 
     def __str__(self):
         return str(self.name)
@@ -127,7 +209,9 @@ def daemon():
     print "Exiting "
 
 
-c = Schedule()
-print c.addJob(daemon).every().lapse
 
+c = Schedule()
+
+c.addJob(daemon).every(20).minutes.monday
+print c.jobs[0].every().crontab
 print ' exit main'
